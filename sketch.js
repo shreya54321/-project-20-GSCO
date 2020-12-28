b5ffb003cc604e16f84faa4e37a404869ff8803f
shreya54321 <75309555+shreya54321@users.noreply.github.com>
2020-12-28 14:13:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var car , wall ;
 var speed , weight ;
 
 function setup() {
-  createCanvas(1600,400); 
+  createCanvas((1000,400); 
 
   car = createSprite(50, 200, 50, 50);
   wall = createSprite(1500,200,60,height/2);
